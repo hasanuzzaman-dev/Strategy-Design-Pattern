1eab6b9f1c936e651a46d4f59386712743b47b8b
Hasan <hasanuzzaman.dev@gmail.com>
2021-06-03 15:22:47 +0600

    আমরা এই ক্লাসগুলোকে আমাদের আগের CheckoutSystem class থেকেই ব্যবহার করব, তবে একটা context class এর সাহায্যে। context class এর কাজ হবে, কোন একটা paymentstrategy কে execute করা। আমরা এইটাকে আলাদা একটা লেয়ারের abstraction হিসেবে চিন্তা করতে পারি।

--- src/methods/PaymentMethods.java
+++ src/methods/PaymentMethods.java
@@ -1,38 +1,15 @@
 package methods;
 
+import strategy.PaymentStrategy;
+
 public class PaymentMethods {
+    PaymentStrategy paymentStrategy;
 
-    private String selectedPaymentMethod;
-    public void setSelectedPaymentMethod(String s){
-        this.selectedPaymentMethod = s;
+    public void setPaymentStrategy(PaymentStrategy paymentStrategy) {
+        this.paymentStrategy = paymentStrategy;
     }
 
-    public void pay() {
-        switch (selectedPaymentMethod) {
-            case "COD" -> payWithCOD();
-            case "Bkash" -> payWithBkash();
-            case "Nagad" -> payWithNagad();
-            case "SureCash" -> payWithSureCash();
-            case "Card" -> payWithCard();
-            case "Rocket" -> payWithRocket();
-        }
-    }
-    private void payWithCOD() {
-        System.out.println("Pay with COD is successful");
-    }
-    private void payWithBkash() {
-        System.out.println("Pay with Bkash is successful");
-    }
-    private void payWithNagad() {
-        System.out.println("Pay with Nagad is Successful");
-    }
-    private void payWithCard() {
-        System.out.println("Pay with Card is Successful");
-    }
-    private void payWithRocket() {
-        System.out.println("Pay with Rocket is Successful");
-    }
-    private void payWithSureCash() {
-        System.out.println("Pay with SureCash is Successful");
+    public void executeStrategy(){
+        paymentStrategy.pay();
     }
 }
